62b85545ecb5034b431e5a0ca127c538260c8d02
MissusConductorX <therewasnosoul@gmail.com>
2026-04-04 17:16:52 -0500

    Add admin command handling in script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function handleCommand(input) {
 
+if (input.toLowerCase() === "admin") {
+  printLine("notice", "Accessing Administrative Wing...");
+  
+  setTimeout(() => {
+    window.location.href = "admin.html";
+  }, 1200);
+}
